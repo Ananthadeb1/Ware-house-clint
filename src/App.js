6b57a1c49c5a0cec8257de441fe644a860ee8afb
Anantha debnath <88941537+Ananthadeb1@users.noreply.github.com>
2022-05-08 17:42:04 +0600

    setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home/Home';
+import Header from './Components/Header/Header';
+import Inventory from './Components/Home/Inventory/Inventory';
+import Footer from './Components/Footer/Footer';
+import NotFound from './Components/Shared/NotFound/NotFound';
+import Login from './Components/Shared/Login/Login';
+import Register from './Components/Shared/Register/Register';
+import SpiceDetail from './Components/SpiceDetail/SpiceDetail';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/spice/:id' element={<SpiceDetail></SpiceDetail>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
