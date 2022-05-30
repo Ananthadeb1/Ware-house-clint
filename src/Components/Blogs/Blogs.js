034f95b40591e6b4d16643296eaf70df01efdab0
Anantha Debnath <ananthadebnath103@gmail.com>
2022-05-30 14:40:42 +0600

    lol

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,7 +4,7 @@ const Blogs = () => {
     return (
         <div>
             <section className='mx-5 px-5 mt-5'>
-                <div><h1>Difference between Node.js and Javascript</h1></div>
+                <div><h1>Difference between Node.js and js</h1></div>
                 <hr className='w-75 mx-auto' />
                 <div className='row'>
                     <div className='col-lg-6 col-sm-12'>
@@ -12,7 +12,7 @@ const Blogs = () => {
                         <p>NodeJS is a cross-platform and opensource Javascript runtime environment that allows the javascript to be run on the server-side. Nodejs allows Javascript code to run outside the browser. Nodejs comes with a lot of modules and mostly used in web development. </p>
                     </div>
                     <div className='col-lg-6 col-sm-12'>
-                        <h4>Javascript</h4>
+                        <h4>Javascript </h4>
                         <p>Javascript is a Scripting language. It is mostly abbreviated as JS. It can be said that Javascript is the updated version of the ECMA script. Javascript is a high-level programming language that uses the concept of Oops but it is based on prototype inheritance.</p>
                     </div>
                 </div>
